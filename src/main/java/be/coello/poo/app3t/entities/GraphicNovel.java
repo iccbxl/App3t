7de82c1cb396a9ebf88d110ad393351fc6a1a7d8
robinson <robincoello@hotmail.com>
2020-05-03 21:59:29 +0200

    mep graphicnovel

--- src/main/java/be/coello/poo/app3t/entities/GraphicNovel.java
+++ src/main/java/be/coello/poo/app3t/entities/GraphicNovel.java
@@ -1,5 +1,43 @@
 package be.coello.poo.app3t.entities;
 
-public class GraphicNovel {
+import java.util.UUID;
 
+public class GraphicNovel extends Book{
+
+	private String catoonist; 
+	private boolean color;
+	
+	
+	public GraphicNovel(UUID id, String title, String author, short totalpages, String langguage, String catoonist) {
+		super(id, title, author, totalpages, langguage);
+		this.catoonist = catoonist;
+		this.color = true;
+	}
+	
+	
+	public String getCatoonist() {
+		return catoonist;
+	}
+	public void setCatoonist(String catoonist) {
+		this.catoonist = catoonist;
+	}
+	
+	
+	public boolean isColor() {
+		return color;
+	}
+	public void setColor(boolean color) {
+		this.color = color;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "GraphicNovel [catoonist=" + catoonist + ", color=" + color + "]";
+	} 
+	
+	
+	
+	
+	
 }
